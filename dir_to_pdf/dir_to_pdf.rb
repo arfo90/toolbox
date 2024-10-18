e8507cc82b6e889843b5ba6050e0ddd6eabaac6f
Amir Fatemi <arfo.amireza@gmail.com>
2024-10-18 17:54:55 +0200

    Added option to skip the images

--- dir_to_pdf/dir_to_pdf.rb
+++ dir_to_pdf/dir_to_pdf.rb
@@ -4,31 +4,40 @@ require 'prawn'
 require 'optparse'
 require 'fileutils'
 
-def process_directory(dir, pdf)
+Encoding.default_external = Encoding::UTF_8
+Prawn::Fonts::AFM.hide_m17n_warning = true
+
+def process_directory(dir, pdf, options)
   Dir.glob(File.join(dir, '**', '*')).sort.each do |file|
     next if File.directory?(file)
-    
+
     begin
       pdf.start_new_page
+      pdf.font 'Helvetica'
       pdf.fill_color '808080'  # Gray color for the file name
       pdf.text File.basename(file), size: 10, align: :left
       pdf.move_down 10
-      pdf.fill_color '000000'  # Reset to black for main content
-
+      pdf.fill_color '000000'
       case File.extname(file).downcase
-      when '.txt', '.md'
-        pdf.text File.read(file)
+      when '.txt', '.md', '.rb', '.css', '.html', '.erb', '.ru', '.js'
+        pdf.start_new_page
+        content = File.read(file, encoding: 'UTF-8')
+        pdf.text content
       when '.jpg', '.jpeg', '.png', '.gif'
-        pdf.image file, fit: [pdf.bounds.width, pdf.bounds.height - 20]  # Adjust height to account for filename
+        if options[:skip_images]
+          puts "Skipping image: #{file}"
+        else
+          pdf.start_new_page
+          pdf.image file, fit: [pdf.bounds.width, pdf.bounds.height]
+        end
       when '.pdf'
         Prawn::Document.new(template: file) do |new_pdf|
-          pdf.go_to_page(pdf.page_count)
           pdf.start_new_page(template: new_pdf.page.dictionary)
         end
       else
         puts "Skipping unsupported file: #{file}"
       end
-    rescue => e
+    rescue StandardError => e
       puts "Error processing file #{file}: #{e.message}"
     end
   end
@@ -36,19 +45,21 @@ end
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: pdf_converter.rb [options]"
-
-  opts.on("-d", "--directories DIR1,DIR2,DIR3", Array, "Directories to process") do |d|
+  opts.banner = 'Usage: pdf_converter.rb [options]'
+  opts.on('-d', '--directories DIR1,DIR2,DIR3', Array, 'Directories to process') do |d|
     options[:directories] = d
   end
-
-  opts.on("-o", "--output FILE", "Output PDF file") do |o|
+  opts.on('-o', '--output FILE', 'Output PDF file') do |o|
     options[:output] = o
+  end
+
+  opts.on('--skip-images', 'Skip images') do |si|
+    options[:skip_images] = true if si
   end
 end.parse!
 
 if options[:directories].nil? || options[:output].nil?
-  puts "Please specify input directories and output file."
+  puts 'Please specify input directories and output file.'
   exit
 end
 
@@ -56,8 +67,9 @@ output_file = options[:output]
 FileUtils.mkdir_p(File.dirname(output_file))
 
 Prawn::Document.generate(output_file) do |pdf|
+  pdf.font 'Helvetica'
   options[:directories].each do |dir|
-    process_directory(dir, pdf)
+    process_directory(dir, pdf, options)
   end
 end
 
